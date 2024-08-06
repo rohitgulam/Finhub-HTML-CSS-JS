b67933f7102805e62f1869c9380bcfaa91ef1c30
Rohit Gulam <rohitsgulam@gmail.com>
2024-08-06 14:32:03 +0300

    loops

---
+++ JS/loops.js
@@ -0,0 +1,62 @@
+// LOOPS 
+
+// For Loops 
+
+// i = i + 1, i++, i +=1
+
+// (start, stop, step)
+
+// Stop - terminating condition
+
+
+// Print from 1 to 100 
+// for (i = 1; i <= 100; i = i + 1 ) {
+//     console.log("I'm sorry " + i);
+// }
+
+// Print even numbers from 0 to 100
+// for (i = 0; i <= 100; i = i + 2 ){
+//     console.log(i);
+// }
+
+
+// Print odd numbers from 0 to 100
+// for (i = 1; i <= 100; i++ ){
+//     console.log(i);
+// }
+
+// While Loop 
+
+// (condition)
+
+// var int = 3
+
+// while(int < 5 ){
+//     console.log('Printing: ' + int);
+
+//     int++
+// }
+
+// Print table 8 upto 8 * 8
+
+var table = 8
+var i = 1
+
+while( i < 9 ){
+    console.log("8 x " + i + " = " + i * 8 );
+
+    // console.log(`8 x ${i} = ${i * 8}`);
+    
+
+    i ++
+    
+}
+
+// Another way to do string concatenation - New Way 
+// Use ${} with backticks ``
+
+// console.log(`8 x ${i} = ${i * 8}`);
+
+
+
+
