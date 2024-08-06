f27b89ac48f08ca34d88a46ef70e32d062eafc01
Rohit Gulam <rohitsgulam@gmail.com>
2024-08-06 12:17:17 +0300

    basic js

---
+++ JS/index.js
@@ -0,0 +1,101 @@
+// Variables and Data types
+
+
+// Naming Variables 
+
+// Dont use spaces 
+
+// Camel case - theLoggedInUser
+
+// Snake case - the_logged_in_user
+
+// Pascal case - TheLoggedInUser
+
+// Kebab case  - the-logged-in-user
+
+
+
+// Data Types 
+
+// Number 
+var myNumber = 2
+
+// String 
+var myString = 'The string'
+
+
+// Boolean 
+var myBool = true
+
+// Array - Always uses square brackets []
+var myArray = [2, 54, 2.5, 454]
+var myArray2 = [2, 54, 2.5, true, 'string']
+
+// Accessing things in Array. Arrays starts at 0
+// console.log(myArray[3]);
+
+// Object - Always uses curly braces {}
+
+// Key - Value
+
+var myObj = {
+    "user1" : 'John Doe',
+    "user2" : 'KJ Apa',
+    "user3" : "Jane Doe"
+}
+
+var myObj2 = {
+    "number" : 1,
+    "array" : [23, 86, 98, ['wee', 'mimi']],
+    "object" : {'key': 'value', 'name': 'Johnny'}
+}
+
+var myUser = {
+    "name" : "James Cam",
+    "age" : 14,
+    "ID" : '2003-03-00318',
+    'status' : 'verified',
+    'paid' : true
+}
+
+// Accesing things in an object - 
+
+// console.log(myObj2.array[3][1]);
+// console.log(myObj2.object.name);
+
+
+// Another way 
+// console.log(myObj=>user1);
+
+
+
+
+// var myNumber = {'name': 'Ju', 'age' : 12, 'verified': true}
+
+
+
+
+
+
+// Operations 
+
+// // Addtion
+// console.log(2 + 10);
+
+// // Substraction
+// console.log(2 - 10);
+
+
+// // Multiplication
+// console.log(2 * 10);
+
+
+// // Division
+// console.log(2 / 10);
+
+// // Modulus - Give the remainder 
+// console.log(10 % 3);
+
+
+// console.log( 2 * 2 + 10);
+
