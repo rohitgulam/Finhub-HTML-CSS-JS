667115357ef0775f5318c885e673d9ba1c5db942
Rohit Gulam <rohitsgulam@gmail.com>
2024-08-07 09:18:43 +0300

    functions

---
+++ JS/functions.js
@@ -0,0 +1,61 @@
+// Functions
+
+// function functionName (parameters) {
+// Code to execute
+// }
+
+// Functions without Arguments/Parameters
+// function printName(){
+//     console.log('Hello, my name is Jimmy');   
+// }
+
+// printName()
+
+// Functions with Arguments/Parameters
+// function printName(name){
+//     console.log('Hello, my name is ' + name);   
+// }
+
+// printName('Jimmy')
+
+
+// function checkAge (age) {
+//     if (age >= 18){
+//         console.log('Age greater than 18');
+        
+//     } else {
+//         console.log("Age less than 18");
+//     }
+// }
+
+// checkAge(9)
+
+
+
+// function checkEven(number){
+//     if (number % 2 === 0) {
+//         return 'Number is even';
+//     }
+
+// }
+
+// 
+
+var age = 12;
+
+if (age >= 18) {
+    welcomeToApp() 
+    
+} else {
+    decline()
+}
+
+function welcomeToApp(){
+    console.log('Hello, welcome');
+}
+
+
+function decline(){
+    console.log('You are not of age yet');
+}
+
