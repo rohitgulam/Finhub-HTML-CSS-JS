add17c6a06935bd5d66142997d00412544771da8
Rohit Gulam <rohitsgulam@gmail.com>
2024-08-07 13:04:03 +0300

    to do list v1

---
+++ todo/script.js
@@ -0,0 +1,102 @@
+const form = document.getElementById('input-form');
+const input = document.getElementById('input');
+
+// THE PARENT FOR ALL ITEMS 
+const listItems = document.getElementById('todo-list-items');
+
+
+
+const todoItems = [];
+
+
+// console.log(listItems);
+
+
+form.addEventListener('submit', saveItem);
+
+
+function saveItem (e) {
+    e.preventDefault()
+    todoItems.push(input.value)
+    input.value = '';
+    console.log(todoItems)
+
+    displayItems()
+}
+
+
+
+
+function displayItems(){
+    // Clear the list 
+    listItems.innerText = ''
+
+    
+    for (var i = 0; i <= todoItems.length - 1; i++) {
+
+        // Div for Item (The PARENT ITEM)
+        var itemDiv = document.createElement('div');
+        itemDiv.classList.add('todo-list-item');
+        
+        // Div for item name 
+        var todoListItemNameDiv = document.createElement('div')
+        todoListItemNameDiv.classList.add('todo-list-item-name')
+        
+        // Paragraph for item title (Child of Item NAme )
+        var todoTitleParagraph = document.createElement('p')
+        todoTitleParagraph.classList.add('todo-title')
+        
+        // Change it's text
+        todoTitleParagraph.innerText = todoItems[i]
+        
+        todoListItemNameDiv.appendChild(todoTitleParagraph)
+        
+        console.log(todoListItemNameDiv);
+        
+        
+        
+        
+        
+        // Div for actions/buttons (THE PARENT FOR BUTTONS)
+        var actionsDiv = document.createElement('div')
+        
+        
+        // Buttons 
+        var editButton = document.createElement('button')
+        editButton.classList.add('icon')
+        
+        // Child of Button
+        var editIcon = document.createElement('i');
+        editIcon.classList.add('fa-solid')
+        editIcon.classList.add('fa-pencil')
+        
+        editButton.appendChild(editIcon);
+        
+        
+        
+        var deleteButton = document.createElement('button')
+        deleteButton.classList.add('icon')
+        
+        var deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('fa-solid')
+        deleteIcon.classList.add('fa-trash')
+        
+        deleteButton.appendChild(deleteIcon);
+        
+        // Append buttons to actionsDiv
+        actionsDiv.appendChild(editButton)
+        actionsDiv.appendChild(deleteButton)
+        
+        // Append divs to todo list item
+        itemDiv.appendChild(todoListItemNameDiv)
+        itemDiv.appendChild(actionsDiv)
+        
+        
+        
+        listItems.appendChild(itemDiv);
+        
+    }
+    
+}
+
+displayItems()
