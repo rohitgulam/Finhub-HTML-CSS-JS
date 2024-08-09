0c0c7b60e015aaac90dc3cd434b42ecafec54374
Rohit Gulam <rohitsgulam@gmail.com>
2024-08-09 15:55:13 +0300

    finish

--- vue/todo-app/src/App.vue
+++ vue/todo-app/src/App.vue
@@ -1,14 +1,56 @@
 <script setup>
-import {ref} from 'vue';
-
+import {ref} from "vue";
+// Vue JS
 var taskInput = ref('');
 
+var itemsArray = ref([
+]);
 
-// console.log(input.value);
+// const items = [1,2,3,4,5,6,7,8];
+// const nums = ['one', 'two', 'three', 'four'];
 
-function submitForm() {
-console.log(taskInput.value);
+// // for (var i = 0; i <= arr.length - 1; i ++ ) {
+// //   console.log(arr[i]);
+// // }
 
+
+// items.forEach( (item, index) => {
+//   console.log(item, index);
+// })
+
+// JS
+// form.addEventListener('submit', saveItem);
+var itemToEdit = ref(null);
+
+
+function saveItem(){
+  // console.log(itemsArray.value);
+  // console.log(taskInput.value);
+  console.log('itemToEdit');
+  console.log(itemToEdit.value);
+  
+  if (itemToEdit.value !== null) {
+    itemsArray.value[itemToEdit.value] = taskInput.value;
+    itemToEdit.value = null;
+  } else {
+    itemsArray.value.push(taskInput.value);
+  }
+  taskInput.value = '';
+
+  // console.log(itemsArray.value);
+  
+}
+
+function deleteItem(index){
+  itemsArray.value.splice(index, 1);
+}
+
+
+function editItem(index) {
+  taskInput.value = itemsArray.value[index];
+  itemToEdit.value = index
+  // console.log(itemToEdit.value);
+  
 }
 
 
@@ -20,7 +62,7 @@ console.log(taskInput.value);
 <div class="container" >
         <h1>To Do App</h1>
 
-        <form v-on:submit.prevent="submitForm" id="input-form">
+        <form v-on:submit.prevent="saveItem" id="input-form">
             <input v-model="taskInput" type="text" name="input" id="input" placeholder="Enter your task here.....">
             <button type="submit">
                 <i class="fa-solid fa-plus"></i>
@@ -29,19 +71,31 @@ console.log(taskInput.value);
 
         <hr >
         <div id="todo-list-items" >
-            <!-- <div class="todo-list-item" >
+            <div class="todo-list-item" v-for="(item, index) in itemsArray" :key="index" >
                 <div class="todo-list-item-name" >
-                    <p class="todo-title" >Create a new desing</p>
+                    <p 
+                    class="todo-title" >
+                      {{ item }}
+                      <!-- {{ index }} -->
+                    </p>
                 </div>
                 <div>
-                    <button id="edit" class="icon edit">
+                    <button @click="editItem(index)" id="edit" class="icon edit">
                         <i class="fa-solid fa-pencil"></i>
                     </button>
-                    <button id="delete" class="icon delete">
+                    <button v-on:click="deleteItem(index)" id="delete" class="icon delete">
                         <i class="fa-solid fa-trash"></i>
                     </button>
                 </div>
+            </div>
+
+
+
+            <!-- <div :class="className" v-for="(num, index) in nums" :key="index" >
+              {{ num }}
+
             </div> -->
+    
         </div>
     </div>
   </div>
